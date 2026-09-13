be0594bf560521004374dfbf95978c8c9f5ea4dd
hendry10021 <hendry10021@gmail.com>
2026-09-13 11:28:25 +0800

    Update to V1.1 - two photo workflow

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
-const CACHE='inbound-v1';
+const CACHE='inbound-v1-1';
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
